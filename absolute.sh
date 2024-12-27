a3984ddccc3ea43c468aecbc6c6c29d5297abc14
JustSmoking <104763679+JustSmoking@users.noreply.github.com>
2024-12-27 17:52:41 +0100

    Update absolute.sh

--- absolute.sh
+++ absolute.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
-
+#Specifier les permissions d'acces avec la commande chmod +x chemin_dacces_fichier 
+#Commande a executer dans le terminal
 echo "Le absolu du dossier courant : $(pwd)"
